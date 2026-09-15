d25c6bf7629d4e20819375b804334ab5abcd434b
TottosZoltan <Tottoszoltan01@gmail.com>
2026-09-15 09:54:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 const menuButton = document.getElementById("menuButton");
+const closeMenu = document.getElementById("closeMenu");
+
+const sideMenu = document.getElementById("sideMenu");
+const menuOverlay = document.getElementById("menuOverlay");
+
+
+// MENÜ MEGNYITÁSA
 
 menuButton.addEventListener("click", function () {
 
-    alert("A menü működik! 🚀");
+    sideMenu.classList.add("open");
+    menuOverlay.classList.add("open");
+
+});
+
+
+// MENÜ BEZÁRÁSA
+
+closeMenu.addEventListener("click", function () {
+
+    sideMenu.classList.remove("open");
+    menuOverlay.classList.remove("open");
+
+});
+
+
+// HÁTTÉRRE KATTINTÁS
+
+menuOverlay.addEventListener("click", function () {
+
+    sideMenu.classList.remove("open");
+    menuOverlay.classList.remove("open");
 
 });
